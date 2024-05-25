67d8b14e35e88252eea35e89371643a77d91df94
martin-joshy <martinjoshy28@gmail.com>
2024-05-25 14:48:50 +0530

    first commit, make the home page design

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
+        <img className='avatar' src="https://occ-0-7113-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABbLV8D54_hMYjfJnFne274NLCCKX_xOJ62um96ZbY2iBIpfdS8sSLC49KZAk6mj3KVF_k3f0BDGNymqNM1WG0s_XOiMCLf67cG7p.png?r=e6c" alt="" />
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/constants/constants.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3/';
+export const API_KEY = '2689b1bdecf84ae2bb354af623513b2f';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
